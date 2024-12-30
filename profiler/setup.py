ca398b72cf7e0856d17fe479d78091990b7ae38e
bangjh0430 <bangjh0430@kaist.ac.kr>
2024-12-30 23:22:37 +0900

    Clean up the code

--- profiler/setup.py
+++ profiler/setup.py
@@ -14,15 +14,6 @@ from torch.utils.cpp_extension import (
 )
 
 
-def get_cuda_bare_metal_version(cuda_dir):
-    raw_output = subprocess.check_output([cuda_dir + "/bin/nvcc", "-V"], universal_newlines=True)
-    output = raw_output.split()
-    release_idx = output.index("release") + 1
-    bare_metal_version = parse(output[release_idx].split(",")[0])
-
-    return raw_output, bare_metal_version
-
-
 def raise_if_cuda_home_none() -> None:
     if CUDA_HOME is not None:
         return
