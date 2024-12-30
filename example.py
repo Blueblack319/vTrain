9a5a0c414b1bd8bebadb0f3ef591734e13d9ff0f
Jehyeon Bang <bangjh0430@kaist.ac.kr>
2024-12-30 23:46:41 +0900

    Update example.py

--- example.py
+++ example.py
@@ -9,13 +9,21 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def main(args):
+    '''
+    Usage:
+        1. single-node configuration example
+        > python example.py -c config/validation/single/config_val_single_0818.json
+
+        2. multi-node configuration example
+        > python example.py -c config/validation/multi/config_val_175B_8_4_16_6.json
+    '''
     config = vTrainConfig.load_from_file(args.config)
 
     sim = vTrain(config)
 
     result, breakdown = sim()
     pred_iter_time = max(result.values())/1000/1000
-    
+    /
     logger.info(f"predicted iteration time: {pred_iter_time:.3f} ms")
 
 if __name__ == "__main__":
